237cf6979ed9002089f394d7d17c33b16cb7f34f
Gyllion van Elderen <gyllion.van.elderen@appstones.nl>
2018-03-28 16:35:12 +0200

    Add project files.

---
+++ PopupTest/PopupTest/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using PopupTest.Views;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PopupTest.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private INavigationService _navigationService;
+
+        public DelegateCommand OpenPopupCommand { get; set; }
+
+        public MainPageViewModel(INavigationService navigationService) 
+            : base (navigationService)
+        {
+            _navigationService = navigationService;
+
+            Title = "Main Page";
+            OpenPopupCommand = new DelegateCommand(OpenPopup);
+        }
+
+        private void OpenPopup()
+        {
+            _navigationService.NavigateAsync(nameof(TestPopup));
+        }
+    }
+}
---
+++ PopupTest/PopupTest/Views/TestPopup.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Rg.Plugins.Popup.Pages;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PopupTest.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TestPopup : PopupPage
+    {
+        public TestPopup()
+        {
+            InitializeComponent();
+        }
+    }
+}
